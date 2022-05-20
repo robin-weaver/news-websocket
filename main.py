0beeeb084486d56e91274d8edd1b05cfb5680b5d
robin-weaver <robinweaver2001@gmail.com>
2022-05-21 00:14:55 +0200

    added twitter scraping

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import discord
 from discord import Message
+from discord.ext import tasks
 import os
 import websockets
 import asyncio
+import snscrape.modules.twitter as snt
+from datetime import datetime
+import pytz
+
+utc = pytz.UTC
+
+twitter_users = ['elonmusk']
+last_tweet = {}
+for user in twitter_users:
+	last_tweet[user] = datetime.utcnow().replace(tzinfo=utc)
 
 d_client = discord.Client()
 
@@ -45,13 +56,25 @@ async def on_message(message: Message):
 	if message.author.id == 708334730457645119 and message.channel.id == 708365137660215330:
 		async for m in message.channel.history(limit=1):  # this is how you can get message content when using a self bot
 			async with websockets.connect('ws://127.0.0.1:4000') as ws:
-				await ws.send(token + m.content)  # a means of authenticating message source
+				await ws.send(token + 'Walter Bloomberg' + m.content)  # a means of authenticating message source
 
 
 @d_client.event
 async def on_connect():
+	check_for_tweets.start()
 	server = Server()
 	await websockets.serve(server.ws_handler, 'localhost', 4000)
+
+
+@tasks.loop(seconds=15)
+async def check_for_tweets():
+	for t_user in twitter_users:
+		for t in snt.TwitterSearchScraper(f'from:{t_user}').get_items():
+			tweet = t
+			break
+		if tweet.date > last_tweet[t_user]:
+			async with websockets.connect('ws://127.0.0.1:4000') as ws:
+				await ws.send(token + f'@{t_user}' + tweet.content)
 
 
 if __name__ == '__main__':
